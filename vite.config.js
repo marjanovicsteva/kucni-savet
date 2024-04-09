0f88799b827de82689c83156f3e931ae2d5045a1
Stevan Marjanovic <marjanovicstevan@outlook.com>
2024-04-09 15:14:46 +0200

    Pokusaj da se sredi problem profilne slike

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
+import { externalizeDeps } from 'vite-plugin-externalize-deps'
 import * as path from 'path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    externalizeDeps()
+  ],
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') }
