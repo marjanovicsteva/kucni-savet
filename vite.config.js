1117660eb6d78d99e7512a8b43e8e19cd7830e1c
Stevan Marjanovic <marjanovicstevan@outlook.com>
2024-04-09 15:20:42 +0200

    Pokusaj novog builda

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
-import { externalizeDeps } from 'vite-plugin-externalize-deps'
 import * as path from 'path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    externalizeDeps()
+    vue()
   ],
   resolve: {
     alias: [
