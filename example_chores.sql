5643c8e3a075f0ba685d853baadde0945e912836
Stevan Marjanović <marjanovicstevan@outlook.com>
2025-07-12 23:38:10 +0200

    Updated ExampleChores query so it doesn't break beacuse of user not found

--- example_chores.sql
+++ example_chores.sql
@@ -1,6 +1,5 @@
 INSERT INTO
-    "public"."chores" (
-        "id",
+    "chores" (
         "name",
         "assignee",
         "frequency",
@@ -8,67 +7,58 @@ INSERT INTO
     )
 VALUES
     (
-        '2',
         'Čišćenje posipa mačkama',
-        'f34ae7d0-005e-440e-8ae5-08722e2b89a2',
+        null,
         'daily',
         null
     ),
     (
-        '3',
         'Hranjenje mačaka',
-        '7e231fcb-1595-4005-99b7-7ce1e39e5cd1',
+        null,
         'daily',
         null
     ),
     (
-        '4',
         'Iznošenje đubreta',
-        'f34ae7d0-005e-440e-8ae5-08722e2b89a2',
+        null,
         'daily',
         null
     ),
-    ('5', 'Kupanje mačaka', null, 'fortnightly', null),
-    ('6', 'Pojenje mačaka', null, null, null),
+    ('Kupanje mačaka', null, 'fortnightly', null),
+    ('Pojenje mačaka', null, null, null),
     (
-        '7',
         'Rasklanjanje veša',
-        'f688fb36-b2ca-43b2-9401-28538472a5a4',
+        null,
         null,
         null
     ),
     (
-        '8',
         'Ribanje kuhinje',
-        '7e231fcb-1595-4005-99b7-7ce1e39e5cd1',
+        null,
         'weekly',
         null
     ),
     (
-        '9',
         'Ribanje kupatila',
-        '7e231fcb-1595-4005-99b7-7ce1e39e5cd1',
+        null,
         'weekly',
         null
     ),
     (
-        '10',
         'Ribanje podova',
-        'f688fb36-b2ca-43b2-9401-28538472a5a4',
+        null,
         'weekly',
         null
     ),
     (
-        '11',
         'Skupljanje đubreta',
-        'f34ae7d0-005e-440e-8ae5-08722e2b89a2',
+        null,
         'daily',
         null
     ),
     (
-        '12',
         'Usisavanje podova',
-        'f688fb36-b2ca-43b2-9401-28538472a5a4',
+        null,
         'weekly',
         null
     );
