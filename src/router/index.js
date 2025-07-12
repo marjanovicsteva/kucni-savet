a7e984b9c8b08220b2d2fe61eb8c780c00071621
Stevan Marjanović <marjanovicstevan@outlook.com>
2025-07-12 23:33:49 +0200

    Fixed typo and added 404 redirect so the app doesn't crash on reload

--- src/router/index.js
+++ src/router/index.js
@@ -2,52 +2,52 @@ import { createRouter, createWebHistory } from "vue-router"
 import { supabase } from "../../utils/supabase"
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.VITE_ENV === 'production' ? '/kucni-savet/' : '/'),
-    routes: [
-        {
-            path: '/',
-            component: () => import('../views/HomeView.vue'),
-            meta: {
-                requiresAuth: true
-            }
-        },
-        {
-            path: '/notifications',
-            component: () => import('../views/AlertsView.vue'),
-            meta: {
-                requiresAuth: true
-            }
-        },
-        {
-            path: '/login',
-            component: () => import('../views/LoginView.vue')
-        },
-        {
-            path: '/profile',
-            component: () => import('../views/ProfileView.vue'),
-            meta: {
-                requiresAuth: true
-            }
-        }
-    ]
+  history: createWebHistory(import.meta.env.VITE_ENV === 'production' ? '/kucni-savet/' : '/'),
+  routes: [
+    {
+      path: '/',
+      component: () => import('../views/HomeView.vue'),
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/notifications',
+      component: () => import('../views/AlertsView.vue'),
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/login',
+      component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: '/profile',
+      component: () => import('../views/ProfileView.vue'),
+      meta: {
+        requiresAuth: true
+      }
+    }
+  ]
 })
 
 const getCurrentUser = async () => {
-        const { data: { user } } = await supabase.auth.getUser()
+  const { data: { user } } = await supabase.auth.getUser()
 
-        return !!user
+  return !!user
 }
 
 router.beforeEach(async (to, from, next) => {
-    if (to.matched.some(record => record.meta.requiresAuth)) {
-        if (await getCurrentUser()) {
-            next()
-        } else {
-            next("/login")
-        }
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (await getCurrentUser()) {
+      next()
     } else {
-        next()
+      next("/login")
     }
+  } else {
+    next()
+  }
 })
 
 export default router
